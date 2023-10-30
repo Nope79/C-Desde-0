efa135b52b589af30eb74dc4bade9f8a89e44bd9
nope79 <nopesep@gmail.com>
2023-10-29 23:07:25 -0600

    Pues aquí mis prácticas de mi primer día con C#, solo es un poco de sintaxis y poco más

---
+++ C#/Primeros pasos/PS_ejemplo/ejemplo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Creado por SharpDevelop.
+ * Usuario: Master79
+ * Fecha: 29/10/2023
+ * Hora: 03:28 p. m.
+ * 
+ * Para cambiar esta plantilla use Herramientas | Opciones | Codificación | Editar Encabezados Estándar
+ */
+using System;
+
+namespace ejemplo
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
